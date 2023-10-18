d059361d317380bc63e29442358c91519b27fe74
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-19 03:47:06 +0600

    get data from database

--- index.js
+++ index.js
@@ -7,28 +7,45 @@ const port = process.env.PORT || 5000;
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gjzbg5x.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3x6azjv.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+
+        await client.connect();
+        const foodCollection = client.db('foodDB').collection('food')
+
+        // insert data to database
+        app.post('/food', async (req, res) => {
+            const newFood = req.body;
+            const result = await foodCollection.insertOne(newFood);
+            console.log(result);
+            res.send(result)
+        })
+        // get data from database
+        app.get('/food', async(req, res)=>{
+            const cursor = foodCollection.find();
+            const result = await cursor.toArray();
+            console.log(result);
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
