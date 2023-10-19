ca9286401a112ef23af7c3300d877cc542a25eb8
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-19 14:47:40 +0600

    find data from database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,6 +39,15 @@ async function run() {
             console.log(result);
             res.send(result)
         })
+        // find data from database
+        app.get('/food/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await foodCollection.findOne(query);
+            res.send(result);
+
+        })
+        // update food data 
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
