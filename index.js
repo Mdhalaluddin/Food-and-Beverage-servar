24401ffe70723afa3cb1f4cd81e8ffc58ba46141
Halal1122 <mdhealaluddin505@gmail.com>
2023-10-23 20:37:09 +0600

    app use cors added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json())
 
 
 
